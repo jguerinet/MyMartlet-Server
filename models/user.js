386bbae2ac531f6f9f835e1482d288b8b6cfaa3c
yulric <yulrics@gmail.com>
2014-09-24 19:50:18 -0400

    Added method to sign up a user

--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ var mongoose = require("mongoose");
 //Module used to hash user passwords
 var bcrypt = require("bcrypt-nodejs");
 
+//The err codes for signning up a user
+var signupErr = require("../err/signup");
+
+//All the authRoles
+var authRoles = require("../constants/roles");
+
 //The schema for a user of this system
 var userSchema = new mongoose.Schema({
 	//The email address of the user with which they will log into the system
@@ -44,6 +50,43 @@ userSchema.statics.generateHash = function(toHash) {
 //Used to check if the user entered password in the login page matches the hashed one in the db
 userSchema.statics.verifyPassword = function(hashedPassword,password) {
 	return bcrypt.compareSync(password,hashedPassword);
+};
+
+//Method to signup a new user
+//The user object should have the following fields: {email:string,password:string,confPassword:string}
+userSchema.statics.signup = function(user,callback) {
+	//Store the mongoose Use model object
+	var User = this;
+
+	//Try to find a user from the db with the same email as the passed one
+	this.findOne({Email:user.email},function(err,userFound) {
+		//If mongoose err pass it to the callback
+		if(err) {
+			return callback(err);
+		}
+		//Otherwise if we found a user with the same email
+		else if(userFound) {
+			//pass a err object with the right err code
+			return callback({err:signupErr.emailExistsErr});
+		}
+		//Check if the 2 passwords entered match. If they don't pass an err object with the right err code
+		else if(user.password != user.confPassword){
+			return callback({err:signupErr.passDoNotMatch});
+		}
+		//Otherwise no problem. Construct a mongoose Uer and sve to the db
+		else {
+			(new User({Email:user.email,Password:User.generateHash(user.password),Auth:authRoles.pending})).save(function(err,userSaved) {
+				//If err saving user pass to the callback
+				if(err) {
+					return callback(err);
+				}
+				//Otherwise pass null as err to the callback
+				else {
+					return callback(null);
+				}
+			});
+		}
+	});
 };
 
 //Make a mongoose model out of the schema
