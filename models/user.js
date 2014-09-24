a29fcfe94981c735814e82528507ea6fed500ca2
yulric <yulrics@gmail.com>
2014-09-23 20:35:27 -0400

    Added generateHash schema static method

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 //get the mongoose object to defined a mongoose schema and model
 var mongoose = require("mongoose");
 
+var bcrypt = require("bcrypt-nodejs");
+
 //The schema for a user of this system
 var userSchema = new mongoose.Schema({
 	//The email address of the user with which they will log into the system
@@ -15,6 +17,11 @@ var userSchema = new mongoose.Schema({
 		required: true
 	}
 });
+
+//Generates a hash for the passed variable. Mainly used to generate a hash for a user's password
+userSchema.statics.generateHash = function(toHash) {
+	return bcrypt.hashSync(toHash,bcrypt.genSaltSync(8),null);
+};
 
 //Make a mongoose model out of the schema
 module.exports = mongoose.model("User",userSchema);
