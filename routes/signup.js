51e6bb0dca2caeb95efa2128a9c0970893962738
yulric <yulrics@gmail.com>
2014-09-23 23:25:41 -0400

    Changed the signup router to call th passport local strategy inside the route handler

--- routes/signup.js
+++ routes/signup.js
@@ -4,10 +4,23 @@ module.exports = function(app,passport) {
 	var signupRouter = require("express").Router();
 
 	//The /signup post handler. The middleware is the passport local-signup strategy to signup new users to the system
-	signupRouter.post("/",passport.authenticate("local-signup"),function(req,res) {
-		//Everything went well
-		//TODO Make proper response
-		res.send(200);
+	signupRouter.post("/",function(req,res,next) {
+		//Call the passport authenticate inside the route handler since we want to send different response when a username is already present
+		passport.authenticate("local-signup",function(err,user,info) {
+			//If sevrer err then send 500
+			if(err) {
+				res.status(500).end();
+			}
+			//If user with that email is already present then send 409 as well as the err code associated with this err
+			else if(user === false) {
+				res.status(409).end(JSON.stringify(info));
+			}
+			//Otheriwse send 200
+			else {
+				//TODO Do a redirect to the login page
+				res.status(200).end();
+			}
+		})(req,res,next);
 	});
 
 	//Use the signupRouter with /signup as the base url
