18a265b85c2c6dd452f8b71fc67a1a939327182c
yulric <yulrics@gmail.com>
2014-09-25 17:37:35 -0400

    Added setSettingsobject and getNewsFeedId methods

---
+++ util/settings.js
@@ -0,0 +1,27 @@
+//The settings document which we will use
+var settings = null;
+
+//The mongoose setting smodel
+var Settings = require("../models/settings");
+
+//Call this function at the beginning when setting suthe serve to ensure tahat we have a settings object
+exports.setSettingsObject = function() {
+	//Get the settings object from the db
+	Settings.findOne({},function(err,settingsFound) {
+		//If err throw it since we need the settings object
+		if(err) {
+			throw err;
+		}
+
+		//Set the settings vavr to the acquired Settings object
+		settings = settingsFound.toObject();
+	});
+};
+
+//Get the next unique NewsFeedId and updates the id in the settigns var and in the db
+exports.getNewsFeedId = function() {
+	var newsFeedId = settings.UniqueIds.NewsFeedId;
+
+	settings.UniqueIds.NewsFeedId+=1;
+	Settings.updateNewsFeedId();
+};
