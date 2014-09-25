56a968b8f226b3149920cdf8f7d70d3027ea3a44
yulric <yulrics@gmail.com>
2014-09-24 22:26:16 -0400

    Added the err codes possible for the signup

---
+++ err/signup.js
@@ -0,0 +1,4 @@
+//The err code to return when a username tries to signup with an email that already exists
+exports.emailExistsErr = 100;
+//The err code for when the passwords do not match
+exports.passDoNotMatch = 101;
