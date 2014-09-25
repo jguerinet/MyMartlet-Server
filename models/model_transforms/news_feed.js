4e29783729c9682a2f1cea30afafb0467583c814
yulric <yulrics@gmail.com>
2014-09-25 19:21:37 -0400

    Added export for the adminTranform

--- models/model_transforms/news_feed.js
+++ models/model_transforms/news_feed.js
@@ -1,5 +1,5 @@
 //The transform to apply when converting docs to objects for the admin system
-var adminTransform = function(doc,ret,options) {
+exports.adminTransform = function(doc,ret,options) {
 	delete ret.__id;
 	delete ret.LastUpdatedBy;
 	delete ret.LastUpdatedOn;
