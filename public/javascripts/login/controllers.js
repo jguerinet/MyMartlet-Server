a126ccb172310260abda3230d1b7ab77eab78052
yulric <yulrics@gmail.com>
2014-09-25 00:14:03 -0400

    Added code to initialise the module and the code to declare constants

---
+++ public/javascripts/login/controllers.js
@@ -0,0 +1,7 @@
+angular.module("mymartlet.login.controllers",[]);
+
+angular.module("mymartlet.login.controllers")
+	.constants("LognConstants",{
+		loginPostUrl: "http://localhost:3000/login"
+	});
+
