668452c819c916ff44d64f89c1020240c13f30f2
yulric <yulrics@gmail.com>
2014-09-27 16:56:41 -0400

    Changed adminTransform to convert the LastUpdatedOn string to EST

--- models/model_transforms/news_feed.js
+++ models/model_transforms/news_feed.js
@@ -1,3 +1,6 @@
+//Gte the moment library to convert the returned tiomes from Zulu to EST
+var moment = require('moment-timezone');
+
 //The transform to apply when converting docs to objects for the admin system
 exports.adminTransform = function(doc,ret,options) {
 	delete ret._id;
@@ -5,6 +8,12 @@ exports.adminTransform = function(doc,ret,options) {
 	delete ret.AddedBy;
 	delete ret.AddedOn;
 	delete ret.__v;
+
+	//Check if the ret object is not null
+	if(ret) {
+		//Set the LastUpdatedOn to an EST string
+		ret.LastUpdatedOn = moment(ret.LastUpdatedOn).tz("America/Toronto").format();
+	}
 };
 
 //The transform to apply when converting docs to objects for the mobile devices
