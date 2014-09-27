7952d78cf7f1f260dec9b421e989b27e17bbdc5c
yulric <yulrics@gmail.com>
2014-09-26 21:24:57 -0400

    Added /api/delete_news_feed route handler

--- routes/admin/api.js
+++ routes/admin/api.js
@@ -39,6 +39,40 @@ module.exports = function(adminRouter) {
 		}
 	});
 
+	//The route handler to deletes a NewsFeed object to the db
+	adminRouter.post("/api/delete_news_feed", userRoles.can("edit"), function(req,res) {
+		//The count of how many items have been removed from the db
+		var numNewsFeedItemsSuccessDelete = 0;
+		//The count of how many items have been tried to removed from the db
+		var numNewsFeedItemsTryDelete = 0;
+
+		//Go throguh the items passed in the request body
+		for(var index=0; index<req.body.newsFeedItems.length; index++) {
+			//Call the method to delete a object to newfeed collection
+			NewsFeed.deleteNewsFeedItem(req.body.newsFeedItems[index],function(err) {
+				//If no err in saving then increment the count of number of items deleted
+				if(!err) {
+					numNewsFeedItemsSuccessDelete++;
+				}
+
+				//Increment the count of the number of tiems tries to delete
+				numNewsFeedItemsTryDelete++;
+
+				//This checks if we tries to delete all the items passed in the request. If we have go inside the if loop
+				if(numNewsFeedItemsTryDelete === req.body.newsFeedItems.length) {
+					//This checks if all the items have been sucessfully deleted. If it is true then send 200
+					if(numNewsFeedItemsSuccessDelete === numNewsFeedItemsTryDelete) {
+						res.status(200).end();
+					}
+					//Otheriwse send 500
+					else {
+						res.status(500).end();
+					}
+				}
+			});
+		}
+	});
+
 	//The route handler to get config object for the admin system
 	adminRouter.get("/api/GetConfig",userRoles.can("view"),function(req,res) {
 		//the config object
