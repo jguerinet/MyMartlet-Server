0a08c11884785d01dd267749ee4c8a217c48e181
yulric <yulrics@gmail.com>
2014-09-27 17:40:49 -0400

    Added err/reset. it will have the err codes for resetting an account's password

---
+++ err/reset.js
@@ -0,0 +1,4 @@
+//The error id when no reset object was found with the passed reset id
+exports.NO_RESET_FOUND = 1;
+//The error id to send back when the time on the reste object has expired
+exports.TIME_EXPIRED = 2;
