1e0c63a0bbfa1d5bb22d5b6c801d5e1643f7c0f4
yulric <yulrics@gmail.com>
2014-09-26 20:40:18 -0400

    Changes /api/save_news_feed route to save array of items

--- routes/admin/api.js
+++ routes/admin/api.js
@@ -7,17 +7,36 @@ module.exports = function(adminRouter) {
 
 	//The route handler to save a NewsFeed object to the db
 	adminRouter.post("/api/save_news_feed", userRoles.can("edit"), function(req,res) {
-		//Call the method to save a object to newfeed collection
-		NewsFeed.saveNewsFeedItem(req.body,req.user.Email,function(err) {
-			//If err send 500
-			if(err) {
-				res.status(500).end();
-			}
-			//Otherwise all good send 200
-			else {
-				res.status(200).end();
-			}
-		});
+		//The count of how many items have been saved to the db
+		var numNewsFeedItemsSuccessSaved = 0;
+		//The count of how many items have been tries to save to the db
+		var numNewsFeedItemsTrySaved = 0;
+
+		//Go throguh the items passed in the request body
+		for(var index=0; index<req.body.newsFeedItems.length; index++) {
+			//Call the method to save a object to newfeed collection
+			NewsFeed.saveNewsFeedItem(req.body.newsFeedItems[index],req.user.Email,function(err) {
+				//If no err in saving then increment the count of number of items saved
+				if(!err) {
+					numNewsFeedItemsSuccessSaved++;
+				}
+
+				//Increment the count of the number of tiems tries to save
+				numNewsFeedItemsTrySaved++;
+
+				//This checks if we tries to save all the items passed in the request. If we have go inside the if loop
+				if(numNewsFeedItemsTrySaved === req.body.newsFeedItems.length) {
+					//This checks if all the items have been sucessfully saved. If it is true then send 200
+					if(numNewsFeedItemsSuccessSaved === numNewsFeedItemsTrySaved) {
+						res.status(200).end();
+					}
+					//Otheriwse send 500
+					else {
+						res.status(500).end();
+					}
+				}
+			});
+		}
 	});
 
 	//The route handler to get config object for the admin system
