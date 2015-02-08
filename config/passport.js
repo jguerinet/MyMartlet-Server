9cd90b7cb3073f1d881dd320c78743bcd1fc29f1
yulric <yulrics@gmail.com>
2015-02-08 15:29:41 -0500

    Rearranged and added comments

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,94 @@
+/**
+ * Configures all the passport strategies
+ * @module config/passport
+ */
+
 //Local username/password strategy used by this system
 var LocalStrategy = require("passport-local").Strategy;
 
 //The mongoose user model object
 var Account = require("../models/account");
 
+/**
+ * The passport strategy for signing up a new user
+ * @type {LocalStrategy}
+ */
+var localSignupStrategy = new LocalStrategy(
+	{
+		//Tell passport to use the email field in the request body as the username for the account
+		usernameField: 'email',
+		//Tell passport to use the password field in the request body as the password for the account
+		passwordField: 'password'
+	},
+	//done has three params:
+	//Err: Pass this into done if the there was a server error or null if the server responded as it should
+	//User: Pass true if everything went smoothly or false if there was an error
+	//UserErr: If User is false then pass the reason into this param.
+	function(email, password, done) {
+		//Call the mongoose method to signup a new user
+		new Account({email: email, password: password}).signup().then(
+			//No errors
+			function() {
+				return done(null, true);
+			},
+			//Errors
+			function(err) {
+				//if it's a string then it's a user error
+				if(typeof err == 'string') {
+					return done(null, false, err);
+				}
+
+				//Otherwise a server or db error
+				return done(err);
+			}
+		);
+	}
+);
+
+/**
+ * The passport strategy for logging in a user
+ * @type {LocalStrategy}
+ */
+var localLoginStrategy = new LocalStrategy(
+	{
+		//Tell passport to use the email field in the request body as the username for the account
+		usernameField: 'email',
+		//Tell passport to use the password field in the request body as the password for the account
+		passwordField: 'password'
+	},
+	//done has three params:
+	//Err: Pass this into done if the there was a server error or null if the server responded as it should
+	//User: Pass the Account to be logged in if everything went smoothly or false if there was an error
+	//UserErr: If User is false then pass the reason into this param.
+	function(email, password, done) {
+		//Call the method to login the user
+		new Account({email: email, password: password}).login().then(
+			//Success
+			function(accountFound) {
+				return done(null, accountFound);
+			},
+			function(err) {
+				//if it's a string then it's a user error
+				if(typeof err == 'string') {
+					return done(null, false, err);
+				}
+
+				//Otherwise a server or db error
+				return done(err);
+			}
+		);
+	}
+);
+
+/**
+ * The function which adds the strategies for signing up a user and logging in a user. Also adds defines the functions
+ * to for associating a user to a session object (deserializeUser) and to add a user to a session (serializeUser).
+ * @param passport {Passport} The object returned by the passport module
+ */
 module.exports = function(passport) {
-	//When a user logs into our system we need to setup a session for them. This method returns the identifier that will be used to find the user
-	//that has logged into the system in the subsequent requests they make. We use the mongoDB id that is generated when creating a new document as the session id
+	//When a user logs into our system we need to setup a session for them. This method returns the identifier that will
+	// be used to find the use that has logged into the system in the subsequent requests they make. We use the
+	// mongoDB id that is generated when creating a new document as the session id
 	passport.serializeUser(function(account, done) {
 		done(null, account._id);
 	});
@@ -20,46 +102,8 @@ module.exports = function(passport) {
 	});
 
 	//The startegy for signing up a new user to the system
-	passport.use("local-signup",new LocalStrategy({
-		//The name of the username field that passport should be looking for
-		usernameField: "email",
-		//The name of the password field that passport should be looking for
-		passwordField: "password",
-		//Pass the req to the callback
-		passReqToCallback: true
-	},function(req,email,password,done) {
-		//Call the mongoose method to signup a new user
-		new Account({email:email, password:password}).signup().then(
-			function(accountCreated) {
-				return done(null, true);
-			},
-			function(err) {
-				if(typeof err == 'string') {
-					return done(null, false, err);
-				}
-
-				return done(err);
-			}
-		);
-	}));
+	passport.use("local-signup", localSignupStrategy);
 
 	//The strategy for logging in a user
-	passport.use("local-login",new LocalStrategy({
-		usernameField: "email",
-		passwordField: "password",
-		passReqToCallback: true
-	},function(req,email,password,done) {
-		new Account({email: email, password: password}).login().then(
-			function(accountFound) {
-				return done(null, accountFound);
-			},
-			function(err) {
-				if(typeof err == 'string') {
-					return done(null, false, err);
-				}
-
-				return done(err);
-			}
-		);
-	}));
+	passport.use("local-login", localLoginStrategy);
 };
