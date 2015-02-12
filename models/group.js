8142d0cff81366999d8b7c5739453e17491d27d2
yulric <yulrics@gmail.com>
2015-02-11 21:28:37 -0500

    Added deleteGroup static method

--- models/group.js
+++ models/group.js
@@ -95,6 +95,67 @@ groupSchema.statics.getGroups = function(query) {
 };
 
 /**
+ * Deletes a Group from the db that matches the passed query. Also removes the references to this Group
+ * from the Account.groups array.
+ * @param query {Object} The query for which Group to remove from the db
+ * @returns {Q.promise.<|Error>}
+ */
+groupSchema.statics.deleteGroup = function(query) {
+	//Make a promise
+	var deferred = q.defer();
+
+	//Store the reference to the Group model
+	var Group = this;
+
+	//Execute the query to remove the Group from the db that meets the query object
+	Group.findOneAndRemove(query).exec().then(
+		//Success
+		function(groupRemoved) {
+			//If a Group was removed
+			if(groupRemoved) {
+				//Update the groups array of all the Accounts that have a reference to the Group
+				mongoose.model('Account').update({'$pull': {groups: groupRemoved.id}}, function (err) {
+					//If error
+					if (err) {
+						//Create the removed Group again
+						Group.create(groupRemoved).then(
+							//Success
+							function() {
+								//Reject the promise with the err that cause the Account update to fail
+								return deferred.reject(err);
+							},
+							//Error
+							function(err) {
+								//Reject the promise with the error
+								return deferred.reject(err);
+							}
+						);
+					}
+					//No error
+					else {
+						//Resolve the promise
+						return deferred.resolve();
+					}
+				});
+			}
+			//No groups were removed
+			else {
+				//Resolve the promise
+				return deferred.resolve();
+			}
+		},
+		//Error
+		function(err) {
+			//Reject the promise with the Error
+			return deferred.reject(err);
+		}
+	);
+
+	//Return the promise
+	return deferred.promise;
+};
+
+/**
  * The mongoose model for a group
  * @type {Group}
  */
