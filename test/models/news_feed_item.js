d2db04360229e7e9fa3919ce7beeb7d67a052de9
yulric <yulrics@gmail.com>
2015-02-15 15:31:41 -0500

    Added tests for the endDate field

--- test/models/news_feed_item.js
+++ test/models/news_feed_item.js
@@ -71,4 +71,16 @@ describe('NewsFeedItem', function() {
 			assert.isTrue(NewsFeedItem.schema.paths.liveDate.options.required);
 		});
 	});
+
+	describe('#endDate', function() {
+		it('should be an instance field', function() {
+			assert.isObject(NewsFeedItem.schema.paths.endDate);
+		});
+		it('should be of type Date', function() {
+			assert.equal(NewsFeedItem.schema.paths.endDate.constructor.name, 'SchemaDate');
+		});
+		it('should have a required option', function() {
+			assert.isTrue(NewsFeedItem.schema.paths.endDate.options.required);
+		});
+	});
 });
