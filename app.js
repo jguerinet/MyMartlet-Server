2e49a15b848b4fd46ff7b40e6811796e58e55614
Julien Guerinet <julien@guerinet.com>
2016-02-08 21:22:57 -0500

    Removed a ton of unneeded code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //This is the framework we will use to make out lives a whole lot easier
 var express = require('express');
 
-//The noejs path module to make paths that are cross platform
+//The nodejs path module to make paths that are cross platform
 var path = require('path');
 
 //Make the default app object without any routes and middlewares added
@@ -11,13 +11,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//Th module we will use to serve a favicon
-var favicon = require('serve-favicon');
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
-
-//Our loggin module
+//Our logging module
 var logger = require('morgan');
 app.use(logger('dev'));
 
@@ -26,37 +20,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-//Module to parse cookies. Required for passport and express-sesion module
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-//Module to add sessions to our app
-var session = require("express-session");
-//Module that will persist session info to our mongoDB database
-var mongoStore = require("connect-mongo")(session);
-//TODO change to use only in routes that require a session cookie
-app.use(session(
-	{
-		secret: 'secret',
-		store: new mongoStore({db: require("mongoose").connection.db}),
-		resave: true,
-		saveUninitialized: true
-	}
-));
-
-//Our authentication module
-var passport = require("passport");
-//COnfigure the passport object with startegies
-require("./config/passport")(passport);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 //Set the folder which has all our static files so that express can handle serving them
 app.use(express.static(path.join(__dirname, 'public')));
 //Add all the routes for our application
-require("./routes")(app,passport);
+require("./routes")(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -88,6 +55,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
