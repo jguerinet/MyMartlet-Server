19042380eaf317c92ecc5e8e80f1e52b59c7143e
Julien Guerinet <julien@guerinet.com>
2016-02-08 22:05:11 -0500

    Added the auth object

---
+++ auth.js
@@ -0,0 +1,18 @@
+var basicAuth = require('basic-auth');
+
+module.exports = function(username, password) {
+    return function(req, res, next) {
+        //Get the basic auth info
+        var user = basicAuth(req);
+
+        //Check that there is a user, username, and password and that they match
+        if (!user || !user.name || !user.pass || user.name !== username ||
+            user.pass !== password) {
+            //If not, unauthorize them
+            res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+            res.status(401).end();
+        } else {
+            return next();
+        }
+    }
+}
