0abb18168637a49cf1c5bed274d53a98b79e2b53
Julien Guerinet <julien@guerinet.com>
2016-02-08 22:37:06 -0500

    Set up the v1 endpoint

--- routes/v1.js
+++ routes/v1.js
@@ -1,38 +1,14 @@
-var configV1 = require('../data/config_v1.json');
+//The v1 data
+var v1 = require('../data/config_v1.json');
+//The authentication module
+var auth = require('../auth')('admin', 'appvelopers');
+//Router instance
+var router = require('express').Router();
 
-module.exports = function(app) {
-	function auth(req, res, next) {
-		var basicAuth = require("basic-auth");
+//Base route is the v1 data
+router.get('/', auth, function(req, res) {
+  res.json(v1);
+});
 
-		function unauthorized(res) {
-			res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-			res.status(401).end();
-		}
-
-        //Get the basic auth info
-		var user = basicAuth(req);
-
-        //Basic auth username and password
-		var username = 'admin';
-		var password = 'appvelopers';
-
-		//Check that there is a user, username, and password and that they match
-		if (!user || !user.name || !user.pass || user.name !== username ||
-            user.pass !== password) {
-			//If not, unauthorize them
-			unauthorized(res);
-		} else {
-			return next();
-		}
-	}
-
-    //Set up the config router
-	var configRouter = require("express").Router();
-	configRouter.use(auth);
-	configRouter.get('/', function(req, res) {
-		res.json(configV1);
-	});
-
-    //Set up the v1 config at the /config endpoint
-	app.use("/config", configRouter);
-};
+//Export the router
+module.exports = router;
