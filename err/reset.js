168802e50e219c93fe2bff4093e630fa6063e751
Julien Guerinet <julien@guerinet.com>
2016-02-08 21:25:44 -0500

    Removed err folder

--- err/reset.js
+++
@@ -1,4 +0,0 @@
-//The error id when no reset object was found with the passed reset id
-exports.NO_RESET_FOUND = 1;
-//The error id to send back when the time on the reste object has expired
-exports.TIME_EXPIRED = 2;
