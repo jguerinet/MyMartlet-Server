4c93799f622e28b9f073afab5bae194d9c280b74
Julien Guerinet <julien@guerinet.com>
2016-02-09 22:11:44 -0500

    Added the v1 of the places, and an endpoint to get to it

--- routes/v1.js
+++ routes/v1.js
@@ -1,14 +1,22 @@
-//The v1 data
-var v1 = require('../data/config_v1.json');
 //The authentication module
 var auth = require('../auth')('admin', 'appvelopers');
 //Router instance
 var router = require('express').Router();
 
+//The v1 config data
+var v1Config = require('../data/v1_config.json');
+//The v1 places data
+var v1Places = require('../data/v1_places.json');
+
 //Base route is the v1 data
 router.get('/', auth, function(req, res) {
-  res.json(v1);
+    res.json(v1Config);
 });
+
+//Places route
+router.get('/places', auth, function(req, res) {
+    res.json(v1Places);
+})
 
 //Export the router
 module.exports = router;
