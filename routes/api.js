1df1350a4848a23ce4e3272b26f21c36298912dd
Julien Guerinet <julien@guerinet.com>
2016-02-09 21:27:24 -0500

    Added the categories route

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@
 var config = require('../data/config.json');
 //Places data
 var places = require('../data/places.json');
+//Categories data
+var categories = require('../data/categories.json');
 //The authentication module
 var auth = require('../auth')('kjw3ro3ijro4wirj', 'p4j2340rf9rifkerv');
 //Router instance
@@ -20,6 +22,11 @@ router.get('/config', auth, function(req, res) {
 //Places route
 router.get('/places', auth, function(req, res) {
 	res.json(places);
+});
+
+//Categories route
+router.get('/categories', auth, function(req, res) {
+	res.json(categories);
 });
 
 //Export the router
