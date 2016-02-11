0768d74b23ac2e19a687850f07fd9abc22cbc3ab
Julien Guerinet <julien@guerinet.com>
2016-02-10 21:16:09 -0500

    Moved the v1 data to the v1 folder

--- routes/v1.js
+++ routes/v1.js
@@ -4,18 +4,18 @@ var auth = require('../auth')('admin', 'appvelopers');
 var router = require('express').Router();
 
 //The v1 config data
-var v1Config = require('../data/v1_config.json');
+var config = require('../data/v1/config.json');
 //The v1 places data
-var v1Places = require('../data/v1_places.json');
+var places = require('../data/v1/places.json');
 
 //Base route is the v1 data
 router.get('/config', auth, function(req, res) {
-    res.json(v1Config);
+    res.json(config);
 });
 
 //Places route
 router.get('/places', auth, function(req, res) {
-    res.json(v1Places);
+    res.json(places);
 })
 
 //Export the router
